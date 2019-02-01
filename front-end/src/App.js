2f648b111c32e3e964c3a3c1585aa41c9db1167c
Mark Thomas <mark.thomas@yoti.com>
2019-02-01 13:45:38 +0000

    mega lint

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,35 +2,36 @@ import React, { PureComponent } from "react";
 import Form from "./components/form/Form";
 import Overlay from "./components/overlay/Overlay";
 import Particles from "./components/particles/Particles";
+
 import "./App.css";
 
 class App extends PureComponent {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">
-            Mark<span className="span-small-amp">&</span>Rhiannon
-          </h1>
-          <h3>Are Getting</h3>
-          <h1>Married</h1>
-        </header>
-        <div className="App-intro">
-          <p className="p-date">
-            7th September<br />2019
-          </p>
-          <p>We'd love to have you there!</p>
-          <p>
-            Just enter your postal address below and we'll send you an
-            invitation...
-          </p>
-        </div>
-        <Form />
-        <Overlay />
-        <Particles />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1 className="App-title">
+                        Mark<span className="span-small-amp">&</span>Rhiannon
+                    </h1>
+                    <h3>Are Getting</h3>
+                    <h1>Married</h1>
+                </header>
+                <div className="App-intro">
+                    <p className="p-date">
+                        7th September<br />2019
+                    </p>
+                    <p>We'd love to have you there!</p>
+                    <p>
+                        Just enter your postal address below and we'll send you
+                        an invitation...
+                    </p>
+                </div>
+                <Form />
+                <Overlay />
+                <Particles />
+            </div>
+        );
+    }
 }
 
 export default App;
