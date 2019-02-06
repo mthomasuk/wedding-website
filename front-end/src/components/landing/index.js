c68869f616e1b705e6a039f3b5c69f3ce1da73f2
markthrone <mpjthomas@gmail.com>
2019-02-06 14:08:54 +0000

    minified images for gifts

--- front-end/src/components/landing/index.js
+++ front-end/src/components/landing/index.js
@@ -1,17 +1,12 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router";
 
-import Form from "../form";
-import Overlay from "../overlay";
-import Particles from "../particles";
-
 import "./Landing.css";
 
 const API_ROOT = "http://localhost:7778";
 
 class Landing extends Component {
     state = {
-        hideForm: false,
         names: [],
     };
 
@@ -28,48 +23,26 @@ class Landing extends Component {
                 .then((json) => {
                     this.setState({
                         names: json.map(u => u.name.split(" ")[0]),
-                        hideForm: true,
                     });
                 });
         }
     }
 
     render() {
-        const { hideForm, names } = this.state;
-        const nameString = names.join(" & ");
+        const { names } = this.state;
+        const nameString = names.length ? names.join(" & ") : "";
 
         return (
             <div className="Landing">
+                <div className="Slash-left" />
+                <div className="Slash-right" />
                 <header className="Landing-header">
                     <h1 className="Landing-title">
-                        Mark
-                        <span className="span-small-amp">&</span>
-                        Rhiannon
+                        {`Hi${nameString ? ` ${nameString}` : ""}!`}
                     </h1>
-                    <h3>Are Getting</h3>
-                    <h1>Married</h1>
                 </header>
-                <div className="Landing-intro">
-                    <p className="p-date">
-                        7th September
-                        <br />
-                        2019
-                    </p>
-                    <p>
-                        We&#8217;d love to have you there
-                        {`${nameString ? ` ${nameString}` : ""}`}
-                        !
-                    </p>
-                    {!hideForm && (
-                        <p>
-                            Just enter your postal address below and we&#8217;ll
-                            send you an invitation...
-                        </p>
-                    )}
-                </div>
-                {!hideForm && <Form />}
-                <Overlay hideForm={hideForm} />
-                <Particles />
+                <div className="Landing-background" />
+                <div className="Slash-bottom" />
             </div>
         );
     }
