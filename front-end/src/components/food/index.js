c85506f96d5d6be22a10ff2c2e81b89617d72f58
Mark Thomas <mthomas@squiz.co.uk>
2019-02-16 11:00:38 +0000

    stuff yeah

---
+++ front-end/src/components/food/index.js
@@ -0,0 +1,114 @@
+import React from "react";
+import uuid from "uuid";
+
+import Button from "../button";
+
+import "./Food.css";
+
+const OPTIONS = {
+    Starter: [{
+        name: "starter-meat",
+        value: "Dressed Dorset crab meat on toast, spring onions & watercress",
+    }, {
+        name: "starter-veggie",
+        value: "Portwood Farm asparagus, mushroom & bernaise sauce (V)",
+    }],
+    Main: [{
+        name: "main-meat",
+        value: "Roasted lamb loin, jersey royals, wild mushrooms, broad beans & rosmary jus",
+    }, {
+        name: "main-veggie",
+        value: "Shitaake mushroom, goat's cheese & truffle oil fritter, roasted veg ratatouille (V)",
+    }],
+    Dessert: [{
+        name: "dessert-tart",
+        value: "Double chocolate tart, orange caramel sauce (V)",
+    }, {
+        name: "dessert-pudding",
+        value: "Summer pudding of forest fruits, mascarpone sorbet, blueberry coulis and white chocolate ganache (V)",
+    }],
+};
+
+const FoodOption = ({
+    course,
+    dinnerChoices,
+    name,
+    option,
+    selectFood,
+}) => (
+    <label
+        className="Food-label"
+        key={uuid()}
+        htmlFor={`${name}-${option.name}`}
+    >
+        <input
+            checked={dinnerChoices[name] && dinnerChoices[name][course] === option.name}
+            className="Food-radio"
+            id={`${name}-${option.name}`}
+            onChange={() => selectFood({
+                course,
+                name,
+                value: option.name,
+            })}
+            name={`${name}-${course}`}
+            type="radio"
+        />
+        {option.value}
+    </label>
+);
+
+const FoodChoice = ({
+    course,
+    dinnerChoices,
+    name,
+    options,
+    selectFood,
+}) => (
+    <div className="Food-choice" key={uuid()}>
+        <h5>{`${course} for ${name}`}</h5>
+        {options.map(option => (
+            <FoodOption
+                course={course}
+                dinnerChoices={dinnerChoices}
+                key={uuid()}
+                name={name}
+                option={option}
+                selectFood={selectFood}
+            />
+        ))}
+    </div>
+);
+
+const Food = ({
+    course,
+    dinnerChoices,
+    names,
+    selectFood,
+    onNext,
+    onBack,
+}) => (
+    <div className="Food-content">
+        {names.map(name => (
+            <FoodChoice
+                course={course}
+                dinnerChoices={dinnerChoices}
+                key={uuid()}
+                name={name}
+                options={OPTIONS[course]}
+                selectFood={selectFood}
+            />
+        ))}
+        <div className="Button-container">
+            <Button
+                title="Back"
+                onClick={onBack}
+            />
+            <Button
+                title="Next"
+                onClick={onNext}
+            />
+        </div>
+    </div>
+);
+
+export default Food;
--- front-end/src/pages/dinner/index.js
+++ front-end/src/pages/dinner/index.js
@@ -2,15 +2,19 @@ import React, { Component } from "react";
 import { withRouter } from "react-router";
 
 import Header from "../../components/header";
+import Food from "../../components/food";
 
 import "./Dinner.css";
 
 const API_ROOT = "http://localhost:7778";
+const COURSES = ["Starter", "Main", "Dessert"];
+
 const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
 class Dinner extends Component {
     state = {
-        dinnerChoices: [],
+        course: COURSES[0],
+        dinnerChoices: {},
         names: [],
     };
 
@@ -26,25 +30,83 @@ class Dinner extends Component {
                 .then(response => response.json())
                 .then((json) => {
                     const names = json.map(u => u.name.split(" ")[0]);
-                    const dinnerChoices = json.map(u => u.food_choices).filter(Boolean);
+                    // const dinnerChoices = json.map(u => u.food_choices).filter(Boolean);
 
                     return this.setState({
-                        dinnerChoices,
+                        // dinnerChoices,
                         names,
                     });
                 });
         }
     }
 
+    selectFood = ({ course, name, value }) => {
+        const { dinnerChoices: initialDinnerChoices } = this.state;
+
+        const dinnerChoices = {
+            ...initialDinnerChoices,
+            [name]: {
+                ...initialDinnerChoices[name],
+                [course]: value,
+            },
+        };
+
+        this.setState({
+            dinnerChoices,
+        });
+    }
+
+    onNext = () => {
+        const { course, dinnerChoices, names } = this.state;
+        const dinnerKeys = Object.keys(dinnerChoices);
+
+        const isCompleted = dinnerKeys.length === names.length && dinnerKeys
+            .sort()
+            .every((value, index) => value === names.sort()[index])
+        && dinnerKeys.every(d => !!dinnerChoices[d][course]);
+
+        const nextCourseIndex = COURSES.indexOf(course) + 1;
+
+        if (isCompleted) {
+            if (nextCourseIndex === COURSES.length) {
+                console.log({
+                    dinnerChoices,
+                });
+            } else {
+                this.setState({
+                    course: COURSES[nextCourseIndex],
+                });
+            }
+        }
+    }
+
+    onBack = () => {
+        const { course } = this.state;
+        const previousCourseIndex = COURSES.indexOf(course) - 1;
+
+        this.setState({
+            course: COURSES[previousCourseIndex],
+        });
+    }
+
     render() {
-        const { dinnerChoices, names } = this.state;
-        console.log({ dinnerChoices, names });
+        const { course, dinnerChoices, names } = this.state;
+        const courseIndex = COURSES.indexOf(course);
+
         return (
             <div className="Dinner">
                 <Header
                     backgroundColour="rgb(243, 203, 126)"
                     showOurFaces={!isMobile}
                     title="Great! Now, the important bits"
+                />
+                <Food
+                    course={course}
+                    dinnerChoices={dinnerChoices}
+                    names={names}
+                    selectFood={this.selectFood}
+                    onNext={this.onNext}
+                    onBack={courseIndex >= 1 ? this.onBack : () => {}}
                 />
             </div>
         );
